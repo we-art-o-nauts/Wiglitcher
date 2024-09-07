c6ee19cc45296cffa5868a274b65af5b9147dd8e
snappy91 <zazzario@virgilio.it>
2024-09-07 15:04:45 +0200

    images

--- pages/improve.py
+++ pages/improve.py
@@ -11,13 +11,13 @@ class Improve(ft.View):
             ft.AppBar(title=ft.Text("Improve"), bgcolor=ft.colors.SURFACE_VARIANT),
             #ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
 
-            ft.Text(f"Improve by AI: LINK"),
+            ft.Text(f"Improve RESOLUTION and DETAIL with AI: LINK"),
 
-            ft.Text(f"Improve text clarity: LINK"),
+            ft.Text(f"Improve TEXT CLARITY with AI: LINK"),
 
-            ft.Text(f"Improve by Cropping a detail: LINK"),
+            ft.Text(f"Improve by CROPPING the relavant regions: LINK"),
 
-            ft.ElevatedButton("Pick files", icon=ft.icons.UPLOAD_FILE, on_click=lambda _: pick_files_dialog.pick_files(allow_multiple=True)),
+            ft.ElevatedButton("UPLOAD TO COMMONS", icon=ft.icons.UPLOAD_FILE, on_click=lambda _: pick_files_dialog.pick_files(allow_multiple=True)),
 
         ]
  
